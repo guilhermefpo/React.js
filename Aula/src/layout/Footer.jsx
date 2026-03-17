c67729785cf572f38f3475ce126804ceb7bb6a59
Guilherme <gfernandoportelaoliveira@gmail.com>
2026-03-17 20:58:19 -0300

    Adicionando a última aula do curso Hora de Codar: teorico

--- Aula/src/layout/Footer.jsx
+++ Aula/src/layout/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import styles from "./Footer.module.css";
 
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className={styles.footer}>
+      <ul>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
+      </ul>
+
+      <p>Nosso rodapé.</p>
+    </footer>
+  );
 }
 
 export default Footer;
